669e0ce1cc003503ce72a468c7123ebcf14820d4
Md. Almas Ali <almaspr3@gmail.com>
2023-05-28 20:39:46 +0600

    Added assignment 4

---
+++ assignment-3/quiz.js
@@ -0,0 +1,42 @@
+// Write a function called "gradeCalculator" that takes a numerical grade (out of 100) as input and returns a letter grade (A, B, C, D, or F) based on the following grading scale:
+
+// A: 90-100
+
+// B: 80-89
+
+// C: 70-79
+
+// D: 60-69
+
+// F: 0-59
+
+// If the input is not a number between 0 and 100, return an error message.
+
+
+// Test Data:
+
+// The function should return "B" when given the input 85.
+
+
+
+// Grade Calculator program
+function gradeCalculator(grade) {
+    if (typeof grade !== 'number' || isNaN(grade) || grade < 0 || grade > 100) {
+        return "Error: Invalid grade. Please enter a number between 0 and 100.";
+    }
+
+    if (grade >= 90) {
+        return "A";
+    } else if (grade >= 80) {
+        return "B";
+    } else if (grade >= 70) {
+        return "C";
+    } else if (grade >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+}
+
+// Checking from test data "grade = 85"
+console.log(gradeCalculator(85));
